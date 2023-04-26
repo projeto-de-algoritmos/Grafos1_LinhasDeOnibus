05dc79d42f58e8ab07a720490e2363f859eff61e
luiza <luizaesteves987@gmail.com>
2023-04-25 21:00:53 -0300

    Criando mapa Co-authored-by: rafaelxavierr <rafaelxcana@gmail.com>

---
+++ linha-onibus/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import MapPage from "./pages/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "AIzaSyDxtBGJ6KCibNUCJjx2sIXSeCzvheud6f0";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
